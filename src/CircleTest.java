4d4a25447d1968be21db9028b01828bdb2ea88cb
Areej Maryam <areejmaryam406@gmail.com>
2025-09-21 12:19:59 +0500

    Initial commit

---
+++ src/CircleTest.java
@@ -0,0 +1,16 @@
+class Circle {
+  double radius=6;
+
+    public double calculatearea() {
+        return Math.PI*radius*radius;
+    }
+    public double calculateperimeter() {
+        return Math.PI*radius*radius;
+    }
+}
+
+public class CircleTest{
+    public static void main(String[] args) {
+
+    }
+}
